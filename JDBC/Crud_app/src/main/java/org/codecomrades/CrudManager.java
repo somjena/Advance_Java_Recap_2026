dafd6cb4d436cd502825ef80db5702d31abacab0
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-12-23 16:25:46 +0530

    Rename CRUD_APP to Crud_app and add CrudManager
    Renamed the project directory and artifactId from CRUD_APP to Crud_app. Added CrudManager.java with basic MySQL insert functionality. Updated pom.xml to include mysql-connector-java dependency. Removed the old Main.java entry point.

---
+++ JDBC/Crud_app/src/main/java/org/codecomrades/CrudManager.java
@@ -0,0 +1,23 @@
+package org.codecomrades;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CrudManager {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        System.out.println("1.Loading the Driver Class");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "root";
+        String password = "root1234";
+        Connection con = DriverManager.getConnection(url,user,password);
+        Statement st =  con.createStatement();
+        String sql = "insert into student values(1,'som',6371326)";
+        boolean bl = st.execute(sql);
+        System.out.println(bl);
+
+        }
+    }
+
