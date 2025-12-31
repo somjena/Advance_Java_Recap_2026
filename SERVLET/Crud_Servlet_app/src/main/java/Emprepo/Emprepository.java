05738159aec6ec1b1475de8cffe6c29be5cb0b24
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-12-31 14:04:08 +0530

    Add image parameter to register method in Emprepository
    The register method in the Emprepository interface now accepts an InputStream image parameter, allowing image data to be included during user registration.

--- SERVLET/Crud_Servlet_app/src/main/java/Emprepo/Emprepository.java
+++ SERVLET/Crud_Servlet_app/src/main/java/Emprepo/Emprepository.java
@@ -2,10 +2,11 @@ package Emprepo;
 
 import entity.User;
 
+import java.io.InputStream;
 import java.sql.ResultSet;
 
 public interface Emprepository {
-    public boolean register(String id,String name,double salary,String address,int age,String email,String gender,String password);
+    public boolean register(String id, String name, double salary, String address, int age, String email, String gender, String password, InputStream image);
     public int getUniqueid();
     public User verifyCredentials(String uname, String password);
 }
